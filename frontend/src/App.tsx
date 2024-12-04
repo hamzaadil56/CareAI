ee5a9e7ca9cedc8f161cfc1e48d85a75d75c4524
Abdur Rehman <107047867+arrehman3@users.noreply.github.com>
2024-12-05 01:08:53 +0500

    routes,landing_page,patient_dahboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import appRouter from "./routes/routes";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="flex flex-col h-screen items-center justify-center">
-        <h1 className="text-2xl font-bold underline">Care AI</h1>
-      </div>
-    </>
+    <RouterProvider router={appRouter}/>
   );
 }
 
