259fd68b993f8710021ab38c982b351382773569
Muhammad Hamza <hamzaadil56@gmail.com>
2024-12-09 05:25:05 +0500

    fixed

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black">
-        <h1 className="text-3xl font-bold mb-6 text-white">Welcome to CareAI</h1>
-        <Link to="/patient" className="bg-blue-600 text-white px-4 py-2 m-2 rounded hover:bg-blue-700">
+      <h1 className="text-3xl font-bold mb-6 text-white">Welcome to CareAI</h1>
+      <Link
+        to="/patient"
+        className="bg-blue-600 text-white px-4 py-2 m-2 rounded hover:bg-blue-700"
+      >
         View Your Treatments
-        </Link>
-        <Link to="/donor" className="bg-blue-600 text-white px-7 py-2 rounded hover:bg-blue-700">
+      </Link>
+      <Link
+        to="/donor"
+        className="bg-blue-600 text-white px-7 py-2 rounded hover:bg-blue-700"
+      >
         View Your Donors
-        </Link>
-  </div>
-  )
-}
+      </Link>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
